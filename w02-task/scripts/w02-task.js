a7257f6f252b8588a7507d9c9fa9b5893eab78f3
kyle-peterson-1 <pet23003@byui.edu>
2024-03-14 22:59:27 -0700

    updates

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,24 +3,24 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = 'Kyle Peterson';
+let fullName = 'Kyle Peterson'
 let currentYear = new Date().getFullYear();
 let profilePicture = 'images/IMG_4157.JPG';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
-const yearElement = document.getElementById('#year'); 
-const imageElement = document.getElementById('imag'); 
+const yearElement = document.getElementById('#year');
+const imageElement = document.getElementById('imag');
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = `<strong>${fullName}</strong>`;
+nameElement.innerHTML = '<strong>${fullName}</strong>';
 yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', 'Profile image of Kyle Peterson');
 
 /* Step 5 - Array */
-const favFood = ['pasta', 'cake', 'sushi', 'pho'];
+const favFood = ['posta', 'cake', 'sushi', 'pho'];
 const newFavFood = 'pizza';
 favFood.push(newFavFood);
 
@@ -28,4 +28,4 @@ foodElement.innerHTML = favFood.join('<br>');
 favFood.shift();
 foodElement.innerHTML += '<br>' + favFood.join('<br>');
 favFood.pop();
-foodElement.innerHTML += '<br>' + favFood.join('<br>'); 
+foodElement.innerHTML += '<br>' + favFood.join('<br>');
